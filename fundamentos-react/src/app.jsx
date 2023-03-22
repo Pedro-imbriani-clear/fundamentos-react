c48bba76fb459d57c136d1936bc83ed760218e00
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-22 12:02:44 -0300

    card#1 component

--- fundamentos-react/src/app.jsx
+++ fundamentos-react/src/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Fragmento from './components/basicos/Fragmento';
+import Card from './components/layout/Card';
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro'
 import ComParemetro from './components/basicos/ComParametro'
@@ -6,11 +8,21 @@ export default () => (
 
     <div>
         <h1>Fundamentos React</h1>
-        <Aleatorio/>
-        <ComParemetro Titulo='Situaçao do aluno  '
+        <Card titulo='#4 Desafio Aleatorio'>
+        <Aleatorio min={1} max={60} />
+        </Card>
+        <Card titulo='# 3Fragmento'>
+        <Fragmento />
+        </Card>
+        <Card titulo='# 2ComParametro'>
+        < ComParemetro Titulo='Situaçao do aluno  '
             aluno=' Pedro'
-            nota={10}></ComParemetro>
+            nota={10}></ComParemetro> 
+        </Card>
+        <Card titulo='# 1Promeiro'>
         <Primeiro></Primeiro>
+        </Card>
+        
     </div>
 
 );
---
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import'./Card.css'
+export default (props) => {
+    return( 
+    <div className='Card'>
+         <div className='Title'>{props.titulo}</div>
+        <div className='Content'>{props.children} </div>
+    </div>
+    )
+}
